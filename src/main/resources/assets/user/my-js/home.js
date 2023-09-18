550931e4e8ef7d6eb34b886019b11fe33e898ee4
quangtuanb3 <quangtuanb3@gmail.com>
2023-09-18 20:14:03 +0700

    commit to merge

--- src/main/resources/assets/user/my-js/home.js
+++ src/main/resources/assets/user/my-js/home.js
@@ -15,13 +15,13 @@ function renderItems(result) {
                                  style="background-image: url(${car.urlImages[0]});">
                             </div>
                             <div class="text">
-                                <h2 class="mb-0"><a href="#">${car.name}</a></h2>
+                                <h2 class="mb-0"><a href="/car-detail/${car.id}">${car.name}</a></h2>
                                 <div class="d-flex mb-3">
                                     <span class="cat">${car.agency}</span>
                                     <p class="price ml-auto">$ ${car.priceDays} <span>/day</span></p>
                                 </div>
-                                <p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Book
-                                    now</a> <a href="#" class="btn btn-secondary py-2 ml-1">Details</a></p>
+                                <p class="d-flex mb-0 d-block"><a href="/car-detail/${car.id}" class="btn btn-primary py-2 mr-1">Book
+                                    now</a> <a href="/car-detail/${car.id}" class="btn btn-secondary py-2 ml-1">Details</a></p>
                             </div>
                         </div>
                     </div>
